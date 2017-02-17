1aebda2e6b9d7a5dd0a0bf2c9536beee66fe21ce
James Iter <james.iter.cn@gmail.com>
2017-02-17 16:45:45 +0800

    同时兼容前端调试和测试脚本

--- main.py
+++ main.py
@@ -104,9 +104,20 @@ def r_before_request():
 @Utils.dumps2response
 def r_after_request(response):
     try:
+        # https://developer.mozilla.org/en/HTTP_access_control
+        # (中文版) https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Access_control_CORS#Access-Control-Allow-Credentials
+        # http://www.w3.org/TR/cors/
         # 由于浏览器同源策略，凡是发送请求url的协议、域名、端口三者之间任意一与当前页面地址不同即为跨域。
-        # response.headers['Access-Control-Allow-Origin'] = '/'.join(request.referrer.split('/')[:3])
-        response.headers['Access-Control-Allow-Origin'] = '*'
+
+        if request.referrer is None:
+            # 跑测试脚本时，用该规则。
+            response.headers['Access-Control-Allow-Origin'] = '*'
+        else:
+            # 生产环境中，如果前后端分离。那么请指定具体的前端域名地址，不要用如下在开发环境中的便捷方式。
+            # -- Access-Control-Allow-Credentials为true，携带cookie时，不允许Access-Control-Allow-Origin为通配符，是浏览器对用户的一种安全保护。
+            # -- 至少能避免登录山寨网站，骗取用户相关信息。
+            response.headers['Access-Control-Allow-Origin'] = '/'.join(request.referrer.split('/')[:3])
+
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Allow-Methods'] = 'HEAD, GET, POST, DELETE, OPTIONS, PATCH, PUT'
         response.headers['Access-Control-Allow-Headers'] = 'X-Request-With, Content-Type'
